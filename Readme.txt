ff61804e0abddd27f79e9b0344655370f6251bc4
san26 <san26@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-26 11:51:03 +0000

    git-svn-id: https://plugins.svn.wordpress.org/google-adsense-ads-by-san/trunk@76001 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Readme.txt
@@ -0,0 +1,56 @@
+=== Google Adsense Ads by SAN ===
+Contributors: SAN - w3cgallery.com
+Plugin URI: http://www.w3cgallery.com/w3c-css/wordpress-google-adsense-ads-by-san
+Donate link: http://windowshostingpoint.com/work/payment/
+Tags: Google Adsense,Affliate ads, banners 
+Requires at least: 1.1
+Tested up to: 2.6.3
+Stable tag: trunk 
+
+Display Google Adsense,Affliate ads on your website easily, just copy and paste. It creates a tab "Google Adsense Ads" in "Settings" or "Options" tab.
+
+== Description ==
+
+It is very easy way to add google adsense codes past into admin panel and those display on front end, but little changes needed
+
+i know some time little change create many problem to some wordpress owner, but im working on it may be next time i will give you 
+
+a solution wihtout any code changes. but this time you have to do that to run it.
+
+
+admin can just copy past google adsense codes, or other affliate banner codes in it.
+
+Paste this code where you want it to display Ads <?php DisplayAds(); ?>
+
+Or you can pass variable too like this default setting <?php featuredpostsList($type = ´SideBar´) ?> for display SideBar ads.
+
+<a href="http://www.w3cgallery.com/w3c-css/wordpress-google-adsense-ads-by-san">for Instructions and help online Please visit.</a>
+If you like this plugin, please leave your comments on w3cgallery.com 
+
+
+== Screenshots ==
+1. Screenshot Admin Area 
+
+<a href="www.w3cgallery.com/w3c-css/wordpress-google-adsense-ads-by-san">click here</a>
+
+
+== Frequently Asked Questions == 
+
+for Instructions and help online Please <a href="http://www.w3cgallery.com/w3c-css/wordpress-google-adsense-ads-by-san">
+Click Here</a>
+
+
+If you like this plugin, please leave your comments on w3cgallery.com
+
+== Installation ==
+
+1. unzip file and upload file google-adsense.php into your /wp-content/plugins/.
+2. Go to your plugins page and activate plugin.
+3. Go to Settings/options >> "Google Adsense Ads" tab to update your google adsense codes into the boxes.
+4. there are 2 boxes, for side bar and ContentTop those for your help only, you can add any size codes in these boxes, but you only need use right codes on your page to display particular banner.
+5. Paste this code where you want it to display Ads <?php DisplayAds(); ?>  for "sidebar" banners
+6. use <?php featuredpostsList($type = ´ContentTop´) ?> for "contenttop" banner
+
+
+
+
---
+++ google-adsense.php
@@ -0,0 +1,101 @@
+<?php 
+/* 
+Plugin Name: Google Adsense Ads 
+Plugin URI: http://www.w3cgallery.com/w3c-css/wordpress-google-adsense-ads-by-san
+Description: Display Google Adsense,Affliate ads on your website easily, just copy and paste. It creates a tab "Google Adsense Ads" in "Settings" or "Options" tab
+Version: 1.0
+Author: SAN - w3cgallery.com & Windowshostingpoint.com
+Author URI: http://www.w3cgallery.com/
+*/
+
+/*  Copyright 2008  SAN - w3cgallery.com & Windowshostingpoint.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; 
+*/
+
+
+// Main function to diplay on front end
+
+function DisplayAds($type='SideBar') {
+global $post, $wpdb, $ads_sidebar, $ads_contenttop;
+// posts_id from database
+if($type=='SideBar') {
+$sidebar = $ads_sidebar['sidebar'];
+ if($sidebar) {
+ echo stripslashes($sidebar);
+  }
+  }else{
+ 
+ $contenttop = $ads_contenttop['contenttop'];
+ if($contenttop) {
+ echo stripslashes($contenttop);
+  } 
+  
+  }	
+
+}
+
+$data = array(
+				'sidebar' 			=> '',
+				'contenttop' 		=> ''
+							);
+$ol_flash = '';
+
+ $ads_sidebar = get_option('ads_sidebar');
+ $ads_contenttop = get_option('ads_contenttop');
+// print_r($posts_google_data);
+// ADMIN PANLE SEETTING
+function posts_google_ads() {
+    // Add new menu in Setting or Options tab:
+    add_options_page('Google Adsense Ads', 'Google Adsense Ads', 8, 'postsgoogleads', 'posts_google_options_page');
+}
+
+
+/* Define Constants and variables*/
+define('PLUGIN_URI', get_option('siteurl').'/wp-content/plugins/');
+
+/* Functions */
+
+function posts_google_options_page() {
+global $ol_flash, $ads_sidebar, $ads_contenttop, $_POST, $wp_rewrite;
+if (isset($_POST['sidebar']) OR isset($_POST['contenttop'])) { 
+	$ads_sidebar['sidebar'] = str_replace('\\','',$_POST['sidebar']);
+	$ads_contenttop['contenttop'] = str_replace('\\','',$_POST['contenttop']);
+	update_option('ads_sidebar',$ads_sidebar);
+	update_option('ads_contenttop',$ads_contenttop);
+	$ol_flash = "Your List has been saved.";
+		}
+
+if ($ol_flash != '') echo '<div id="message"class="updated fade"><p>' . $ol_flash . '</p></div>';
+
+echo '<div class="wrap">';
+		echo '<h2>Google Adsense Ads by SAN</h2>';
+		echo "<table class='optiontable form-table'><form action='' method='post'>
+		<tr><td colspan='2'><strong>This plugin gives option to copy and paste your ad codes (like: google adsense, affliate codes etc.) and banner display on your website.</strong></td></tr>
+		<tr><td><strong>SideBar</strong></td><td><textarea rows='6' name='sidebar' cols='43'>" . htmlentities($ads_sidebar['sidebar']) . "</textarea></td></tr>
+		<tr><td><strong>ContentTop</strong></td><td><textarea rows='6' name='contenttop' cols='43'>" . htmlentities($ads_contenttop['contenttop']) . "</textarea></td></tr>
+		</table>";
+				
+echo '<Div class="submit"><input type="submit" value="Save your Ads" /></div>
+<p>Paste this code where you want it to display Ads <strong>&lt;?php DisplayAds(); ?&gt;</strong> <br/> Or you can pass variable too like this default setting <strong>&lt;?php featuredpostsList($type = &acute;SideBar&acute;) ?&gt;</strong> for display SideBar ads.</p>
+		</form>';
+echo '<p><a href="http://www.w3cgallery.com/w3c-css/wordpress-google-adsense-ads-by-san">for Instructions and help online Please visit.</a> <br/>
+If you like this plugin, please leave your comments on <a href="http://www.w3cgallery.com/w3c-validate/w3c-blog">w3cgallery.com</a> & <a href="http://www.Windowshostingpoint.com">Windowshostingpoint.com</a></p>';
+		echo '</div>';
+
+}
+
+add_action('admin_menu', 'posts_google_ads');
+
+?>
